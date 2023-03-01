121b534ca34cf557a6e13528c37d6f5d27833b6d
wrinklykong <johnwjones1337@gmail.com>
2023-03-01 11:20:46 -0600

    Movement, TileMap start
    Really basic movement, TIleMap things

---
+++ Game/Assets/CharacterMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speed = 3.0f;
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVertical = Input.GetAxis ("Vertical");
+
+        rb2d.velocity = new Vector2(speed * moveHorizontal, speed * moveVertical);
+
+    }
+}
