0941fd8ef80a8e664ba3798197d44df1eb603cf0
wrinklykong <johnwjones1337@gmail.com>
2023-05-02 00:07:12 -0500

    Functional trees when talking to NPC

--- Game/Assets/DialogueScript.cs
+++ Game/Assets/DialogueScript.cs
@@ -12,6 +12,16 @@ public class DialogueScript : MonoBehaviour
     private Canvas canvas;
     public Canvas inventoryCanvas;
 
+    private GameObject option1;
+    private GameObject option2;
+
+    private Button option1button;
+    private Button option2button;
+    private Image option1image;
+    private Image option2image;
+    private Text option1text;
+    private Text option2text;
+
     public CharacterMovement cm;
 
     void Start()
@@ -21,7 +31,35 @@ public class DialogueScript : MonoBehaviour
         DialogueContainerImage = GetComponentInChildren<Image>();
         canvas = GetComponent<Canvas>();
         canvas.enabled = false;
-        //updateBox("Hello!");
+
+        option1 = GameObject.FindGameObjectsWithTag("option1")[0];
+        option2 = GameObject.FindGameObjectsWithTag("option2")[0];
+        option1button = option1.GetComponentInChildren<Button>();
+        option2button = option2.GetComponentInChildren<Button>();
+
+        option1image = option1.GetComponentInChildren<Image>();
+        option2image = option2.GetComponentInChildren<Image>();
+
+        option1text = option1.GetComponentInChildren<Text>();
+        option2text = option2.GetComponentInChildren<Text>();
+    }
+
+    public void hideOptions() {
+        option1button.enabled = false;
+        option2button.enabled = false;
+        option1image.enabled = false;
+        option2image.enabled = false;
+        option1text.enabled = false;
+        option2text.enabled = false;
+    }
+
+    private void showOptions() {
+        option1button.enabled = true;
+        option2button.enabled = true;
+        option1image.enabled = true;
+        option2image.enabled = true;
+        option1text.enabled = true;
+        option2text.enabled = true;
     }
 
     private void destroyDialogueQueue() {
@@ -40,17 +78,45 @@ public class DialogueScript : MonoBehaviour
         Debug.Log("Hello");
         try {
             string nextD = (string)DialogueList.Dequeue();
-            updateBox(nextD);
-            cm.startTalkingToNPC();
-            inventoryCanvas.enabled = false;
+            if ( nextD == "OPTION" ) {
+                // option1.SetActive(true);
+                // option2.SetActive(true);
+                //string option1String = (string)DialogueList.Dequeue();
+                //string option2String = (string)DialogueList.Dequeue();
+                //string[] option1split = option1String.Split(char.Parse(","));
+                //string[] option2split = option2String.Split(char.Parse(","));
+                option1text.text = (string)DialogueList.Dequeue();
+                //opt1Pos = int.Parse(option1split[1]);
+                option2text.text = (string)DialogueList.Dequeue();
+                //opt2Pos = int.Parse(option2split[1]);
+                nextD = (string)DialogueList.Dequeue();
+                updateBox(nextD);
+                cm.startTalkingToNPC();
+                inventoryCanvas.enabled = false;
+                showOptions();
+            }
+            else if ( nextD == "END" ) {
+                updateBox("NULL");
+                hideBox();
+                cm.stopTalkingToNPC();
+                inventoryCanvas.enabled = true;
+                return;
+            }
+            else {
+                updateBox(nextD);
+                cm.startTalkingToNPC();
+                inventoryCanvas.enabled = false;
+            }
         }
         catch (Exception e) {
+            //Debug.Log(e);
             updateBox("NULL");
             hideBox();
             cm.stopTalkingToNPC();
             inventoryCanvas.enabled = true;
         }
     }
+
 
     void updateBox( string a ) {
         showBox();
@@ -69,6 +135,32 @@ public class DialogueScript : MonoBehaviour
         Canvas[] allCanvases = inventoryCanvas.GetComponentsInChildren<Canvas>();
         foreach ( Canvas a in allCanvases ) {
             a.enabled = true;
+        }
+    }
+
+    public void OptionClicked(int option) {
+        // option = 1 or 2
+        string optType = (option==1) ? "OPT1" : "OPT2";
+        Debug.Log(optType);
+        Debug.Log("Option 1 clicked");
+        // go until option 1
+        hideOptions();
+        string nextD = (string)DialogueList.Dequeue();
+        try {
+            while ( nextD != optType ) {
+                nextD = (string)DialogueList.Dequeue();
+            }
+            nextD = (string)DialogueList.Dequeue();
+            updateBox(nextD);
+            cm.startTalkingToNPC();
+            inventoryCanvas.enabled = false;
+        }
+        catch (Exception e) {
+            Debug.Log(e);
+            updateBox("NULL");
+            hideBox();
+            cm.stopTalkingToNPC();
+            inventoryCanvas.enabled = true;
         }
     }
 
